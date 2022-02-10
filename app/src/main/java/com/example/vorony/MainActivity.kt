f6d369c29998afb401134222e4f5dd3ad7eaf041
astokarev <sandrotokarev@gmail.com>
2022-02-10 20:31:46 +0300

    upd1
    restart button added

--- app/src/main/java/com/example/vorony/MainActivity.kt
+++ app/src/main/java/com/example/vorony/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     private var counterP1: Int = 0
@@ -14,19 +15,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val rootLayout: ConstraintLayout = findViewById(R.id.layout_root)
         val img1: ImageView = findViewById(R.id.starP1)
         val img2: ImageView = findViewById(R.id.starP2)
-        val textP1: TextView = findViewById(R.id.voronyP1)
-        val textP2: TextView = findViewById(R.id.voronyP2)
         val buttonP1: Button = findViewById(R.id.countP1)
         val buttonP2: Button = findViewById(R.id.countP2)
-        buttonP1.setOnClickListener{
-            textP1.text = "Я насчитал ${++counterP1} ворон"
-            if (counterP1 == 100) img1.visibility = View.VISIBLE;
+        val buttonRestart: Button = findViewById(R.id.restartBtn)
+
+        buttonP1.setOnClickListener {
+            buttonP1.text = "Я насчитал ${++counterP1} ворон"
+            if (counterP1 == 100) {
+                buttonRestart.visibility = View.VISIBLE
+                img1.visibility = View.VISIBLE;
+                rootLayout.setBackgroundColor(resources.getColor(R.color.purple_500))
+            }
         }
-        buttonP2.setOnClickListener{
-            textP2.text = "Я насчитал ${++counterP2} ворон"
-            if (counterP2 == 100) img2.visibility = View.VISIBLE;
+
+        buttonRestart.setOnClickListener {
+            buttonP1.text = "Я насчитал 0 ворон"
+            buttonP2.text = "Я насчитал 0 ворон"
+            counterP1 = 0
+            counterP2 = 0
+            img1.visibility = View.INVISIBLE;
+            img2.visibility = View.INVISIBLE;
         }
+        buttonP2.setOnClickListener {
+            buttonP2.text = "Я насчитал ${++counterP2} ворон"
+            if (counterP2 == 100) {
+                buttonRestart.visibility = View.VISIBLE
+                img2.visibility = View.VISIBLE
+                rootLayout.setBackgroundColor(resources.getColor(R.color.teal_200))
+            }
+        };
     }
 }
